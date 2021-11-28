a268697e9a301286d572796e17fa3b613f8a618d
KevinPouzaud <70631924+KevinPouzaud@users.noreply.github.com>
2021-11-28 21:17:03 +0100

    Modifications + Ajouts d'éléments d'apparences
    - Modifications du theme et des couleurs.
    - Modifications des string, textes de l'interface de base.
    - Modifications des logos + utilisation de SVG.
    - Ajout de la bibliothèque RecyclerView pour liste adaptative.

--- Application/app/src/main/java/com/boatList/listofboat/MainActivity.kt
+++ Application/app/src/main/java/com/boatList/listofboat/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.boatList.listofboat
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -53,5 +56,20 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    fun dontClickMe(view: View) {
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setMessage("Why are you clicking on me ?")
+            .setCancelable(true)
+            .setPositiveButton("Sorry", DialogInterface.OnClickListener {
+                    dialog, id -> dialog.cancel()
+            })
+            .setNegativeButton("IDK", DialogInterface.OnClickListener {
+                    dialog, id -> dialog.cancel()
+            })
+        val alert = dialogBuilder.create()
+        alert.setTitle("Don't click on me please.")
+        alert.show()
     }
 }
